ee4ba859fcc550b7b64655f4fc75a7ad3f97cccc
Josh Kline <jkline@okta.com>
2013-04-26 19:30:14 +0000

    Vars for repeated files

--- package.sh
+++ package.sh
@@ -7,19 +7,24 @@ DIR=$(dirname -- $(readlink --canonicalize --no-newline -- $0))
 
 cd $DIR
 
+gemspec=okta_hello.gemspec
+rpmspec_template=rubygem19-okta_hello.spec.in
+rpmspec=rubygem19-okta_hello.spec
+
 # Make gem
-gemout=$(gem1.9 build okta_hello.gemspec)
+gemout=$(gem1.9 build $gemspec)
 
 # What file and version of gem was just build?
 gemfile=$(echo "$gemout" | awk '/File:/ { print $2 }' )
 
 # Interpolate the rpm spec for the new gem
-gem2rpm -t rubygem19-okta_hello.spec.in -o rubygem19-okta_hello.spec $gemfile
+gem2rpm -t $rpmspec_template -o $rpmspec $gemfile
 
 # rpmbuild needs to find the gem
 cp $gemfile $HOME/rpmbuild/SOURCES/
+cp $rpmspec_template $HOME/rpmbuild/SOURCES/
 
 # Boom!
-rpmbuild -ba rubygem19-okta_hello.spec
+rpmbuild -ba $rpmspec
 
 
